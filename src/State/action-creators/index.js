cc939ac3debaf37a1283440ab6fd8ab3636f3904
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2022-03-30 15:05:24 +0530

    video 75 commpleted

---
+++ src/State/action-creators/index.js
@@ -0,0 +1,18 @@
+export const depositeMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'deposit',
+            payload: amount,
+        })
+    }
+}
+
+
+export const withdrawMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'withdraw',
+            payload: amount,
+        })
+    }
+}
---
+++ src/State/reducers/amountReducer.js
@@ -0,0 +1,9 @@
+export default reducer = (state=0, action)=> {
+    if (action.type == 'deposite') {
+        return state + action.payload;
+    } else if (action.type == 'withdraw') {
+        return state - action.payload;
+    }else{
+        return state;
+    }
+}
