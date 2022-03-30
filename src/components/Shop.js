020c44a8215a4a655711eb375b713c53403e7022
AlokKumarRai <alok20530@iiitkalyani.ac.in>
2022-03-30 14:44:49 +0530

    video 74 commpleted

---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Shop() {
+    return (
+        <>
+            <h2 className='mt-4'>Buy Nike Shose of 50Rs</h2>
+            <button className="btn btn-primary mx-2">-</button>
+            Add this item to cart
+            <button className="btn btn-primary mx-2">+</button>
+        </>
+    )
+}
